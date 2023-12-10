cac961087cb1a28f86e40cdad37761b1bc30bc18
SagarMemane <108060123+sagarmemane135@users.noreply.github.com>
2023-12-10 20:35:55 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -48,4 +48,4 @@ app.post('/delete-todo/:index', (req, res) => {
     res.redirect('/');
 });
 
-app.listen(3000, () => console.log('Server started on port 3000'));
+app.listen(8000, () => console.log('Server started on port 8000'));
